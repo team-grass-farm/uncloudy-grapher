be66501938d706e601faade8f11403c70055a1e7
Jaycol Kim <jaycol.kim@gmail.com>
2022-07-19 22:19:59 +0900

    feat: Model data fetcher, pod, and node resources
    Co-authored-by: Han <myungsangBaek@users.noreply.github.com>

--- src/models/Node.ts
+++ src/models/Node.ts
@@ -1,10 +1,6 @@
 interface Node {
   id: string;
   region: 'seoul' | 'busan';
-  CPUUsage: number;
-  memoryUsage: number;
-  CPUSpec: number;
-  memorySpec: number;
   os:
     | 'rhel7'
     | 'rhel8'
@@ -17,25 +13,34 @@ interface Node {
 }
 
 namespace Node {
+  export interface Metric {
+    time: number;
+    values: {
+      CPUUsage: number;
+      memoryUsage: number;
+      CPUSpec: number;
+      memorySpec: number;
+    };
+  }
   /** Prometheus HTTP API Response
    * @see https://prometheus.io/docs/prometheus/latest/querying/api/
    */
-  interface Response {
-    status: 'success';
-    data: {
-      resultType: 'vector';
-      result: [
-        {
-          metric: {
-            __name__: string;
-            job: string;
-            instance: string;
-          };
-          value: [number, string];
-        }
-      ];
-    };
-  }
+  // interface Response {
+  //   status: 'success';
+  //   data: {
+  //     resultType: 'vector';
+  //     result: [
+  //       {
+  //         metric: {
+  //           __name__: string;
+  //           job: string;
+  //           instance: string;
+  //         };
+  //         value: [number, string];
+  //       }
+  //     ];
+  //   };
+  // }
 }
 
 export default Node;
