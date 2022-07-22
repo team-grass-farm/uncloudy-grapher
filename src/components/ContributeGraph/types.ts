f50ff80517ddfc4d0ab4afdc89ff8df9e0ed090a
Jaycol Kim <jaycol.kim@gmail.com>
2022-07-22 20:12:33 +0900

    refactor: Merge multiple usePainters & usePainterEvent into one

--- src/components/ContributeGraph/types.ts
+++
@@ -1,6 +0,0 @@
-import type { Page } from '~models';
-
-export interface Props extends Page.ComponentProps<'div'> {
-  id?: string;
-  data: number[];
-}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as ContributeBox } from './ContributeBox';
-export { default as ContributeGraph } from './ContributeGraph';
 export { default as DevelopmentOnlyAlert } from './DevelopmentOnlyAlert';
 export { default as Grass } from './Grass';
 export { default as UncloudyGraph } from './UncloudyGraph';
