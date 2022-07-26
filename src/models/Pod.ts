94f26062a0ccfd70628866cf050876c7b661bf69
Jaycol Kim <jaycol.kim@gmail.com>
2022-07-26 18:29:54 +0900

    feat: Model admin & developer views

--- src/models/Pod.ts
+++ src/models/Pod.ts
@@ -15,28 +15,6 @@ namespace Pod {
       memorySpec: number;
     };
   }
-
-  /**
-   * Kube-state-metrice
-   * @description kube_pod_container_info
-   */
-  // interface Response {
-  //   status: 'success';
-  //   data: {
-  //     resultType: 'vector';
-  //     result: [
-  //       {
-  //         metric: {
-  //           __name__: 'kube_pod-';
-  //           container: string;
-  //           container_id: string;
-  //           image: string;
-  //           image_id;
-  //         };
-  //       }
-  //     ];
-  //   };
-  // }
 }
 
 export default Pod;
