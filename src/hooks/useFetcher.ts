07344267a5855aa1b07981b7e2a69c0f2c98e8ff
Jaycol Kim <jaycol.kim@gmail.com>
2022-08-13 15:50:54 +0900

    Revert "draft: Apply new Map model & Disable Group settings on useFetcher"
    This reverts commit 6b91b93b81d8f5eefcde69df7a0f9e85e0968dd9.

--- src/hooks/useFetcher.ts
+++ src/hooks/useFetcher.ts
@@ -5,46 +5,45 @@ export default (
   type: 'production' | 'development' | 'offline' = 'production',
   enableSamples?: boolean
 ): [Resource.Map] => {
-  const [pods, setPods] = useState<Map<string, Resource.Pod>>(new Map());
-  const [nodes, setNodes] = useState<Map<string, Resource.Node>>(new Map());
-  const [deployments, setDeployments] = useState<
-    Map<string, Resource.Deployment>
-  >(new Map());
-  const [namespaces, setNamespaces] = useState<Map<string, null>>(new Map());
-  const [clusters, setClusters] = useState<Map<string, Resource.Cluster>>(
-    new Map()
-  );
+  const [pods, setPods] = useState<Resource.Pod[]>([]);
+  const [nodes, setNodes] = useState<Resource.Node[]>([]);
+  const [deployments, setDeployments] = useState<Resource.Deployment[]>([]);
+  const [namespaces, setNamespaces] = useState<string[]>([]);
+  const [clusters, setClusters] = useState<Resource.Cluster[]>([]);
 
   useEffect(() => {
     if (type === 'offline') {
-      enableSamples && setPods(SAMPLE_PODS);
-      enableSamples && setNodes(SAMPLE_NODES);
+      // setPods(enableSamples ? SAMPLE_PODS : []);
+      // setNodes(enableSamples ? SAMPLE_NODES : []);
     }
   }, [enableSamples]);
 
   useEffect(() => {
-    // const info: Set<[string, string]> = new Set();
-    // const deployments: Resource.Deployment[] = [];
-    // const namespaces: string[] = [];
-    // pods.forEach((pod) => info.add([pod.deploymentId, pod.namespace]));
-    // Array.from(info).forEach((val) => {
-    //   deployments.push({
-    //     id: val[0],
-    //     shortId: val[0].split('-deployment-')[0],
-    //     replicas: 0,
-    //     availableReplicas: 0,
-    //     namespace: val[1],
-    //   });
-    //   namespaces.push(val[1]);
-    // });
-    // setDeployments(deployments);
-    // setNamespaces(namespaces);
+    const info: Set<[string, string]> = new Set();
+    const deployments: Resource.Deployment[] = [];
+    const namespaces: string[] = [];
+
+    pods.forEach((pod) => info.add([pod.deploymentId, pod.namespace]));
+    Array.from(info).forEach((val) => {
+      deployments.push({
+        id: val[0],
+        shortId: val[0].split('-deployment-')[0],
+        replicas: 0,
+        availableReplicas: 0,
+        namespace: val[1],
+      });
+      namespaces.push(val[1]);
+    });
+
+    setDeployments(deployments);
+    setNamespaces(namespaces);
   }, [pods]);
 
   useEffect(() => {
-    // const info: Set<string> = new Set();
-    // nodes.forEach((node) => info.add(node.region));
-    // setClusters(Array.from(info).map((val) => ({ id: val })));
+    const info: Set<string> = new Set();
+
+    nodes.forEach((node) => info.add(node.region));
+    setClusters(Array.from(info).map((val) => ({ id: val })));
   }, [nodes]);
 
   return [{ pods, nodes, deployments, namespaces, clusters }];
