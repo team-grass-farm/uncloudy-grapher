2ff544f016975a452b55145c447e193c80517604
Jaycol Kim <jaycol.kim@gmail.com>
2022-08-14 12:59:20 +0900

    style: Remove `import React` on useHooks

--- src/hooks/useFetcher.ts
+++ src/hooks/useFetcher.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SAMPLE_NODES, SAMPLE_PODS } from '~constants';
 
 export default (
--- src/hooks/usePainterEvent.ts
+++ src/hooks/usePainterEvent.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { report } from '~utils/logger';
 import { getCursorPosition } from '~utils/positioner';
 
