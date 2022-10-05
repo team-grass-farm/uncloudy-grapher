88e0c1e7014354839716c16589c4a74a865ae929
Jaycol Kim <jaycol.kim@gmail.com>
2022-10-05 22:01:13 +0900

    feat: Apply new usePainterEvent returned values on usePainter() & Change params of clearRendered()

--- src/hooks/usePainter.ts
+++ src/hooks/usePainter.ts
@@ -1,16 +1,9 @@
-import { RefObject, useCallback, useEffect, useRef, useState } from 'react';
-import { isNull } from 'util';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { usePainterEvent } from '~hooks';
 import { report } from '~utils/logger';
 import {
-  clearRendered,
-  renderBlocks,
-  renderGrids,
-  renderGroups,
-  renderHighlightedBlocks,
-  renderHighlightedPoints,
-  renderPoints,
-  translate,
+    clearRendered, renderBlocks, renderGrids, renderGroups, renderHighlightedBlocks,
+    renderHighlightedPoints, renderPoints, translate
 } from '~utils/painter';
 import { getGridPositions, getPointPositions } from '~utils/positioner';
 
@@ -23,19 +16,16 @@ export default (): [
   React.Dispatch<React.SetStateAction<Dimensions>>,
   React.Dispatch<React.SetStateAction<Painter.FlagMap>>
 ] => {
-  const [isDevMode] = useState(process.env.NODE_ENV === 'development');
   const [level, setLevel] = useState<1 | 2 | 3>(2);
   const [dimensions, setDimensions] = useState<Dimensions>({
     width: 0,
     height: 0,
   });
-  const [
-    eventRef,
-    perspective,
-    highlightedPointPosition,
-    highlightedBlockPositions,
-    setLevelOnEvent,
-  ] = usePainterEvent(dimensions);
+  const [renderedPlot, setRenderedPlot] = useState<Positioner.Plot | null>(
+    null
+  );
+  const [eventRef, perspective, highlightedPointPosition, setLevelOnEvent] =
+    usePainterEvent(dimensions);
 
   const refMap: Painter.RefMap = {
     ...(isDevMode && {
@@ -151,6 +141,37 @@ export default (): [
   );
 
   useEffect(() => {
+    const ctx = refMap.event.current?.getContext('2d');
+    if (!!!ctx) return;
+    else if (highlightedPointPosition === null) {
+      clearRendered(ctx);
+    }
+
+    if (!!highlightedPointPosition && visible.points) {
+      if (visible.points) {
+        renderHighlightedPoints(ctx, [highlightedPointPosition]);
+      }
+
+      if (renderedPlot) {
+        const { row, column } = highlightedPointPosition;
+        const highlightedBlock = renderedPlot.blocks.data.get([row, column]);
+
+        if (!!highlightedBlock) {
+          renderHighlightedBlocks(ctx, {
+            ...renderedPlot.blocks,
+            data: new Map([
+              [[row, column] as [number, number], highlightedBlock],
+            ]),
+          });
+          report.warn('usePainter', ['rendered.']);
+        }
+      }
+      // renderHighlightedBlocks(ctx, highlightedBlockPositions);
+    }
+  }, [highlightedPointPosition, renderedPlot]);
+
+  useEffect(() => {
+    report.debug('usePainter', [{ perspective }]);
     translate(
       refMap.blocks.current?.getContext('2d'),
       snapshotMap.blocks,
@@ -162,25 +183,6 @@ export default (): [
       perspective
     );
   }, [perspective]);
-
-  useEffect(() => {
-    const ctx = refMap.event.current && refMap.event.current.getContext('2d');
-    if (!refMap.event.current) return;
-    else if (highlightedPointPosition === null) {
-      clearRendered(ctx, refMap.event.current);
-    }
-
-    if (!!highlightedPointPosition && visible.points) {
-      renderHighlightedPoints(ctx, [highlightedPointPosition]);
-    }
-    if (!!highlightedBlockPositions) {
-      report.log('usePainter', [
-        'executed renderHighlightedBlocks(): ',
-        highlightedBlockPositions,
-      ]);
-      renderHighlightedBlocks(ctx, highlightedBlockPositions);
-    }
-  }, [highlightedPointPosition, highlightedBlockPositions]);
 
   return [
     refMap,
