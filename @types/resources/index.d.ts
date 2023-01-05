2591507ca160d07db6279b3b4d78abbf98b79926
Jaycol Kim <jaycol.kim@gmail.com>
2023-01-05 23:15:27 +0900

    draft: Add k8s api on @types/resources

--- @types/resources/index.d.ts
+++ @types/resources/index.d.ts
@@ -11,6 +11,19 @@ declare namespace Resource {
     id: string;
   }
 
+  /**
+   * @draft designed for k8s  api
+   */
+  /*
+  interface API {
+    apiVersion: string;
+    kind: string;
+    metadata: any;
+    spec: any;
+    status: any;
+  }
+  */
+
   interface Deployment {
     id: string;
     shortId: string;
@@ -63,5 +76,47 @@ declare namespace Resource {
         memorySpec: number;
       };
     }
+
+    /**
+     * @draft designed for k8s pod api
+     */
+    /*
+    interface API extends Resource.API {
+      apiVersion: 'v1';
+      kind: 'Pod';
+      metadata: {
+        annotations?: Record<string, any>;
+        creationTimestamp?: string;
+        generateName?: string;
+        labels?: Record<string, any>;
+        name: string;
+        namespace: string;
+        resourceVersion: string;
+        uid: string;
+      };
+      spec: {
+        containers?: any[];
+        dnsPolicy?: string;
+        nodeName: string;
+        restartPolicy: string;
+      };
+      status: {
+        conditions?: Array<{
+          lastProbeTime: string | null;
+          lastTransitionTime: string;
+          status: 'True' | 'False';
+          type: string;
+        }>;
+        containerStatuses?: any[];
+        hostIP?: string;
+        phase?: 'Pending' | 'Running' | 'Succeeded' | 'Failed' | 'Unknown';
+      };
+    }
+
+    interface Spec {
+      activeDeadlineSeconds?: number;
+      affinity: Affinity;
+    }
+  */
   }
 }
